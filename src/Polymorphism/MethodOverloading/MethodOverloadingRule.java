70448bf5102b49c84c3e9f2316311ca1d6578749
Avinash <surwaseavinash85@gmail.com>
2025-09-12 11:20:02 -0700

    Modified with some readable print statements

--- src/Polymorphism/MethodOverloading/MethodOverloadingRule.java
+++ src/Polymorphism/MethodOverloading/MethodOverloadingRule.java
@@ -2,20 +2,31 @@ package Polymorphism.MethodOverloading;
 
 public class MethodOverloadingRule {
     private void run(){
-        System.out.println("this is private method");
+//        System.out.println("this is private method");
+        System.out.println("MethodOverloadingRule.run | private without param");
     }
 
     public void run(int a){
-        System.out.println("RUn overloaded method");
+//        System.out.println("overloaded method");
+        System.out.println("MethodOverloadingRule.run | a = "+a);
     }
+
 
    protected static void play(){
         System.out.println("play cricket");
     }
 
     private static void play(int a){
-        System.out.println("....");
+//        System.out.println("This is static method with parameter "+a);
+        System.out.println("MethodOverloadingRule.play | a = "+a);
     }
 
+    public static void main(String[] args) {
+
+        MethodOverloadingRule obj = new MethodOverloadingRule();
+        obj.run();
+        obj.run(11);
+        MethodOverloadingRule.play(22);
+    }
 
 }
