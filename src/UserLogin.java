9fe0ff1ec90ba8824c1983b388c054fb2f94177e
Avinash <surwaseavinash85@gmail.com>
2025-09-18 21:10:26 -0700

    UserLogin Basic Logic

---
+++ src/UserLogin.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UserLogin {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int setId=101;
+        String setPassword="avi@123";
+
+        while (true){
+            System.out.println("Enter UserId");
+            int id=sc.nextInt();
+            sc.nextLine();
+            System.out.println("Enter Password");
+            String password=sc.nextLine();
+            if (setId==id && setPassword.equals(password)){
+                System.out.println("Login Successful..");
+                break;
+            }else {
+                System.out.println("Incorrect Please Try again...!");
+            }
+        }
+    }
+}
