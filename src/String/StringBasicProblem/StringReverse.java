33d60ab617905db56b86baddd42340d666ec8eac
Avinash <surwaseavinash85@gmail.com>
2025-09-24 22:13:58 -0700

    ..

--- src/String/StringBasicProblem/StringReverse.java
+++ src/String/StringBasicProblem/StringReverse.java
@@ -3,5 +3,19 @@ package String.StringBasicProblem;
 public class StringReverse {
     public static void main(String[] args) {
         String s="Avinash";
+        String tem="";
+        for (int i=s.length()-1;i>=0;i--){
+            tem+=s.charAt(i);
+        }
+        System.out.println(tem);
+    }
+}
+
+//convert char[] to String
+
+class ABCD{
+    public static void main(String[] args) {
+        char[]arr={'A','v','i','n','a','s','h'};
+        System.out.println(arr);
     }
 }
