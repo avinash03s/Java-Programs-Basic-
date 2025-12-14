26319c70bca5d676a3dd3d921d894db0c50c56ca
Avinash <surwaseavinash85@gmail.com>
2025-12-13 22:47:48 -0800

    added code

--- src/Multithreading/ThreadYield.java
+++ src/Multithreading/ThreadYield.java
@@ -15,6 +15,7 @@ class MainThreadYield{
     public static void main(String[] args) {
         ThreadYield ob= new ThreadYield();
         ob.start();
+        System.out.println("Commiting code to github");
         for (int i = 0; i < 5;i++){
             System.err.println("Main Thread");
         }
