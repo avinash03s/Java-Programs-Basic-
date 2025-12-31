b12d6c8f2a22d103bdaa50d708a44f4f216f3302
Avinash <surwaseavinash85@gmail.com>
2025-12-31 16:42:13 +0530

    Some code

--- src/CollectionFramwork/ArrayListToArray.java
+++ src/CollectionFramwork/ArrayListToArray.java
@@ -1,4 +1,69 @@
 package CollectionFramwork;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+//Converting ArrayList to Array
+
 public class ArrayListToArray {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("10");
+        list.add("20");
+        list.add("30");
+
+        String arr[] = new String[list.size()];
+        list.toArray(arr);
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+}
+
+//Converting Array to ArrayList
+//using Collection.addAll();
+class ArrayToArrayList {
+
+    public static void main(String[] args) {
+        String arr[] = new String[]{"Avinash", "Rama", "Rahul"};
+
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, arr);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
+
+//Converting Array to ArrayList
+//using Arrays.asList();
+class ArrayToArrayList2 {
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"A", "B", "C", "D", "E"};
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
+
+//Converting Array to ArrayList
+//using ArrayList.addAll();
+class ArrayToArrayList3 {
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"Z", "Y", "X"};
+
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList(arr));
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
 }
--- src/Multithreading/DeadLock99.java
+++ src/Multithreading/DeadLock99.java
@@ -1,4 +1,41 @@
 package Multithreading;
 
+//DeadLock....!
+
 public class DeadLock99 {
+
+    static String s1 = "Hello";
+    static String s2 = "Java";
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (s1) {
+                    System.out.println("Thread One => Resources s1");
+                    synchronized (s2) {
+                        System.out.println("Thread One => Resource s2");
+                    }
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (s2) {                                     //replace s1
+                    System.out.println("Thread Two => Resource s1");
+                    synchronized (s1) {                                  //replace s2
+                        System.out.println("Thread Two => Resource s2");
+                    }
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
 }
+//How to avoid DeadLock
+//1)maintain resources order;
+//2)if possible to avoid nested synchronized block;
