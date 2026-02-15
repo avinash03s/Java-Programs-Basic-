2f1a05d05bc43a924f9ff782f276a25dcaf4e518
Avinash <surwaseavinash85@gmail.com>
2026-02-15 10:58:59 +0530

    java 8 realtime problem

---
+++ src/Java_8_feature/SolvingRealTimeQueries/customer/CustomerMain.java
@@ -0,0 +1,26 @@
+package Java_8_feature.SolvingRealTimeQueries.customer;
+
+public class CustomerMain extends CustomerQuestionMethods{
+
+    public static void main(String[] args) {
+        question1();
+        System.out.println("-------------------------------------------");
+        question2();
+        System.out.println("---------------------------------------------");
+        question3();
+        System.out.println("---------------------------------------------");
+        question4();
+        System.out.println("---------------------------------------------");
+        question5();
+        System.out.println("-----------------------------------------------");
+        question6();
+        System.out.println("-----------------------------------------------");
+        question7();
+        System.out.println("-----------------------------------------------");
+        question8();
+        System.out.println("-----------------------------------------------");
+        question9();
+        System.out.println("-----------------------------------------------");
+        question10();
+    }
+}
